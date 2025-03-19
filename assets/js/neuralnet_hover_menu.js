599c62d993dfd9fea2f15858da1af62420ceb8be
Jacob Dichter <58149811+jacobdichter@users.noreply.github.com>
2025-03-19 13:52:24 -0400

    Add JS code for hover menu behavior

---
+++ assets/js/neuralnet_hover_menu.js
@@ -0,0 +1,25 @@
+// Await full page load to run script
+document.addEventListener("DOMContentLoaded", function () {
+    const navImage = document.getElementById("nav-image")
+    const imageMap = {
+        home: "assets/network_drawing_red.svg",
+        articles: "assets/network_drawing_purple.svg",
+        resume: "assets/network_drawing_green.svg",
+        contact: "assets/network_drawing_purple.svg"
+    };
+    // Select all menu items
+    const menuItems = document.querySelectorAll(".menu-item");
+
+    // Add event listeners to each menu-item
+    menuItems.forEach(item => {
+        item.addEventListener("mouseover", function () {
+            // Return ID of hovered menu-item
+            navImage.src = imageMap[this.id];
+        });
+
+        item.addEventListener("mouseout", function () {
+            //Reset to grey network menu
+            navImage.src = "assets/network_drawing_grey.svg";
+        });
+    });
+});
