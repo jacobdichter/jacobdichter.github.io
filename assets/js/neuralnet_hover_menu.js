578dcab527af283fd17c94198151abf908fbeba5
Jacob Dichter <58149811+jacobdichter@users.noreply.github.com>
2025-03-19 15:56:52 -0400

    Update neuralnet_hover_menu.js

--- assets/js/neuralnet_hover_menu.js
+++ assets/js/neuralnet_hover_menu.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Reset image only when leaving the entire menu area
     menuContainer.addEventListener("mouseleave", function () {
-        navImage.src = "assets/network_drawing_BASE.svg"; // Change image back
+        navImage.src = "assets/network_drawing_blue.svg"; // Change image back
 
         const menuItems = document.querySelectorAll(".menu-item");
         menuItems.forEach(function (item) {
