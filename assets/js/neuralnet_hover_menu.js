1edec2293703548fa12649560712a38947a92b21
Jacob Dichter <58149811+jacobdichter@users.noreply.github.com>
2025-03-19 14:46:58 -0400

    Update neuralnet_hover_menu.js

--- assets/js/neuralnet_hover_menu.js
+++ assets/js/neuralnet_hover_menu.js
@@ -21,10 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (event.target.classList.contains("menu-item")) {
             // Change neuralnet image
             navImage.src = imageMap[event.target.id];
-            
-            // Apply the font color and background color changes
-            const styles = hoverStyles[event.target.id];
-            event.target.style.color = styles.color;
         }
     });
 
@@ -32,13 +28,5 @@ document.addEventListener("DOMContentLoaded", function () {
     menuContainer.addEventListener("mouseleave", function () {
         navImage.src = "assets/network_drawing_grey.svg"; // Change image back
         
-        // Reset styles for each item in the imageMap
-        Object.keys(imageMap).forEach(function (key) {
-            const menuItem = document.getElementById(key);
-            if (menuItem) {
-                menuItem.style.color = ""; // Reset text color
-                menuItem.style.backgroundColor = ""; // Reset background color
-            }
-        });
     });
 });
