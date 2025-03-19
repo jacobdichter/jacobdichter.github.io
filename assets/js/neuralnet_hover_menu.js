f520eac3a8afdcc7826e2c546c9afb6be100f886
Jacob Dichter <58149811+jacobdichter@users.noreply.github.com>
2025-03-19 14:00:11 -0400

    Update neuralnet_hover_menu.js

--- assets/js/neuralnet_hover_menu.js
+++ assets/js/neuralnet_hover_menu.js
@@ -1,25 +1,23 @@
 // Await full page load to run script
 document.addEventListener("DOMContentLoaded", function () {
     const navImage = document.getElementById("nav-image")
+    const menuContainer = document.querySelector(".nav-links")
     const imageMap = {
         home: "assets/network_drawing_red.svg",
         articles: "assets/network_drawing_purple.svg",
         resume: "assets/network_drawing_jadegreen.svg",
         contact: "assets/network_drawing_blue.svg"
     };
-    // Select all menu items
-    const menuItems = document.querySelectorAll(".menu-item");
-
-    // Add event listeners to each menu-item
-    menuItems.forEach(item => {
-        item.addEventListener("mouseover", function () {
-            // Return ID of hovered menu-item
-            navImage.src = imageMap[this.id];
-        });
-
-        item.addEventListener("mouseout", function () {
-            //Reset to grey network menu
-            navImage.src = "assets/network_drawing_grey.svg";
-        });
+    // Change image when hovering over a menu item
+    menuContainer.addEventListener("mouseover", function (event) {
+        // Check if the hovered item is a menu link
+        if (event.target.classList.contains("menu-item")) {
+            navImage.src = imageMap[event.target.id];
+        }
     });
+
+    // Reset image only when leaving the entire menu area
+    menuContainer.addEventListener("mouseleave", function () {
+        navImage.src = "assets/network_drawing_grey.svg";
+        });
 });
